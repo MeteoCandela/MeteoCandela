925a59f89a4ba576aefee24ecbe9de1566d83d1d
MeteoCandela <cartanya.cat@gmail.com>
2026-02-17 21:22:16 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — MeteoValls (scope-safe + anti-stale ESM + PUSH)
 
-const VERSION = "2026-02-16-010"; // 🔁 PUJA AIXÒ SEMPRE
+const VERSION = "2026-02-17-001"; // 🔁 PUJA AIXÒ SEMPRE
 const CACHE_PREFIX = "meteovalls-";
 const CACHE_NAME = `${CACHE_PREFIX}${VERSION}`;
 
