8969ed86a119cef2cf6390ed0d92afec88c069d1
MeteoCandela <cartanya.cat@gmail.com>
2026-02-19 20:53:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js — entrypoint únic (ESM)
-export const V = "2026-02-18-001";
+export const V = "2026-02-19-001";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
