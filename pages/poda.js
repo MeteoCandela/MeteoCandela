c8e9573ff9f6b2c5f131f78fea384f866df9eb1f
MeteoCandela <cartanya.cat@gmail.com>
2026-02-28 11:14:33 +0100

    Create poda.js

---
+++ pages/poda.js
@@ -0,0 +1,9 @@
+{
+  "months": ["Gen","Feb","Mar","Abr","Mai","Jun","Jul","Ago","Set","Oct","Nov","Des"],
+  "rows": [
+    {"label":"Hivern / estructural","key":"winter","on":[1,1,1,0,0,0,0,0,0,0,1,1]},
+    {"label":"Floració (arbustos/enfiladisses)","key":"bloom","on":[0,1,1,0,1,1,0,0,0,0,0,0]},
+    {"label":"Estiu / control de vigor","key":"summer","on":[0,0,0,0,0,0,1,1,1,0,0,0]},
+    {"label":"Retoc estètic (bardisses/geranis)","key":"touch","on":[0,0,1,0,1,1,0,0,1,1,0,0]}
+  ]
+}
